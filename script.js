da055c83bb27bdfc6fcdfa5b9d7420151f2688ec
Adriana Kuczaj <ada.kuczaj@gmail.com>
2023-08-01 22:41:31 +0200

    refactoring js functions

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-let heightInput = document.querySelector(".js-height");
-let weightInput = document.querySelector(".js-weight");
-let form = document.querySelector(".js-form");
-let bmiResult = document.querySelector(".js-bmiResult");
+{
+    const calculateBMI = () => {
+        const heightInput = document.querySelector(".js-height");
+        const weightInput = document.querySelector(".js-weight");
 
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
+        const height = heightInput.value;
+        const weight = weightInput.value;
 
+        return weight / ((height / 100) ** 2);
+    };
 
-    let height = heightInput.value;
-    let weight = weightInput.value;
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        const result =  calculateBMI();  
+        updateResultText(result);
+    };
 
-    let result = weight / ((height / 100) ** 2);
+    const updateResultText = (result) => {
+        const bmiResult = document.querySelector(".js-bmiResult");
+        bmiResult.innerText = result.toFixed(2);
+    }
 
-    bmiResult.innerText = result.toFixed(2);
-})
+    const init = () => {
+        const form = document.querySelector(".js-form");
+        form.addEventListener("submit", onFormSubmit);
+    }
+
+    init();
+}
